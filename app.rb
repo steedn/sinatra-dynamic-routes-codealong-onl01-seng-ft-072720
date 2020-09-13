b70e3017b89da733ebcc3b58b119dce4458c94a5
Nathan Steed <steedn@outlook.com>
2020-09-13 03:08:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,8 @@ class App < Sinatra::Base
   get '/multiply/:num1/:num2' do
     @num1 = params[:num1]
     @num2 = params[:num2]
-    "#{@num1*@num2}"
+    sum = @num1 * @num2
+    sum
   end
 
 end
